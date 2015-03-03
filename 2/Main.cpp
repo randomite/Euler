f0f10e23ff20afdaf8b68a166d2b13e7e98938e5
Ravin Sardal <ravin.sardal@gmail.com>
2015-03-02 21:01:26 -0800

    solution 2

---
+++ 2/Main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+int main(int argc, char *argv[]) {
+	int sum = 0;
+	int y = 1; 
+	int z = 2;
+	sum = 2;
+	for (int x=3;x<=4000000;x=y+z) {
+		y=z;
+		z=x;
+		if (x%2 == 0) {
+			sum += x;
+		}
+	}
+	cout << sum;
+}
