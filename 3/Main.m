48049ad3a7519f54ca20af5ff0c18ee993217567
Ravin Sardal <ravin.sardal@gmail.com>
2015-03-03 18:23:56 -0800

    Algorithm optimization

--- 3/Main.m
+++ 3/Main.m
@@ -7,7 +7,8 @@ What is the largest prime factor of the number 600851475143 ?
 #import <Foundation/Foundation.h>
 
 bool checkIfPrime(long y){
-	for(long x=2l;x<y;x++) {
+	long x;
+	for(x=2l;x<y;x++) {
 		if (y%x == 0) {
 			return false;
 		}
@@ -19,14 +20,13 @@ bool checkIfPrime(long y){
 int main(int argc, char *argv[]) {
 	@autoreleasepool {
 		long DIVIDEND = 600851475143;
-			long largestPrimeFactor = 0;
-			
-			for(long x=DIVIDEND;x>2;x--) {
-				if (DIVIDEND%x == 0 && checkIfPrime(DIVIDEND/x)==true) {
-					largestPrimeFactor = DIVIDEND/x;
-				}		
-			}
-			NSLog(@"%d",largestPrimeFactor);
-			
+		long largestPrimeFactor = 0;
+		long x; 	
+		for(x=DIVIDEND;x>2;x--) {
+			if (DIVIDEND%x == 0 && checkIfPrime(DIVIDEND/x)==true) {
+				largestPrimeFactor = DIVIDEND/x;
+			}		
+		}
+		NSLog(@"%d",largestPrimeFactor);
 	}
 }
